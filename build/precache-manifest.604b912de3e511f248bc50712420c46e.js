10427eba20e90029a2e6811082b4266f271434e1
Arshansh <arshanshagarwal1@gmail.com>
2021-05-21 20:44:19 +0530

    finishing project

--- build/precache-manifest.604b912de3e511f248bc50712420c46e.js
+++ build/precache-manifest.604b912de3e511f248bc50712420c46e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ff7c5546fae242b1bb2833c05712b953",
+    "revision": "e395d2392582dc78274c89d49bfcfc7c",
     "url": "/SQL-Editor/index.html"
   },
   {
-    "revision": "cfa4a11f2e82052a6c79",
-    "url": "/SQL-Editor/static/css/main.b0bc64bb.chunk.css"
+    "revision": "6bc41992551d5af4b012",
+    "url": "/SQL-Editor/static/css/main.de6da1ca.chunk.css"
   },
   {
     "revision": "06119e33fab45d903436",
     "url": "/SQL-Editor/static/js/2.f9d82fd4.chunk.js"
   },
   {
-    "revision": "cfa4a11f2e82052a6c79",
-    "url": "/SQL-Editor/static/js/main.d7e4e71a.chunk.js"
+    "revision": "6bc41992551d5af4b012",
+    "url": "/SQL-Editor/static/js/main.f0b8a4e6.chunk.js"
   },
   {
     "revision": "c322e0dbe3883c79ebb0",
