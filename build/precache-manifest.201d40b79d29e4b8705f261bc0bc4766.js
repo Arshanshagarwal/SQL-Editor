72d6339b26db3af01ad78629f92fc7b68f9879c8
Arshansh <arshanshagarwal1@gmail.com>
2021-05-22 22:46:07 +0530

    Final Commit for atlan

--- build/precache-manifest.201d40b79d29e4b8705f261bc0bc4766.js
+++ build/precache-manifest.201d40b79d29e4b8705f261bc0bc4766.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "536df69b3f264f24f696d5dde6088aa2",
+    "revision": "a47f3c5b07bfa9260ebc79ff4587fb83",
     "url": "/SQL-Editor/index.html"
   },
   {
-    "revision": "ec39b8b21d6e655897ad",
+    "revision": "a648cf5a5eb411c28f2d",
     "url": "/SQL-Editor/static/css/main.32f0775a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SQL-Editor/static/js/2.f9d82fd4.chunk.js"
   },
   {
-    "revision": "ec39b8b21d6e655897ad",
-    "url": "/SQL-Editor/static/js/main.b0dd5849.chunk.js"
+    "revision": "a648cf5a5eb411c28f2d",
+    "url": "/SQL-Editor/static/js/main.bc89d724.chunk.js"
   },
   {
     "revision": "c322e0dbe3883c79ebb0",
